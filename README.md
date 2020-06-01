a6e085098b005e05b4b0124b3acce1c3f76fab93
Niraj Palecha <nirajftw@gmail.com>
2020-06-01 12:21:32 +0530

    add release.sh and move to date based versioning

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # git-playground
+
+repo to play with git and hub commands
+
---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if ! [ -x "$(command -v hub)" ]; then
+    echo "hub not found on PATH, go to https://hub.github.com/"
+    exit -1
+fi
+
+TAG=$(date +"r%Y.%m.%d")
+
+hub release create $TAG
+git pull
